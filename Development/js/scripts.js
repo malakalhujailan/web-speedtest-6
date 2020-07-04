38b9a308beeae083b3894dde0ad69724296bd356
malakalhujailan <66518892+malakalhujailan@users.noreply.github.com>
2020-07-04 17:13:42 +0300

    Update scripts.js

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,19 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+    if ( name = prompt("enter name please"));
+ {console.log}
+}
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+    if ( n / 2 == 0);
+    console.log( true) ;
+    if ( n / 2 == 1);
+    console.log( false) 
+}
